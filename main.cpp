960812b0b550c308d6001b251f72ee7f895055dd
theultraman20 <32559038+theultraman20@users.noreply.github.com>
2020-10-13 21:03:47 -0400

    Update main.cpp
    This didn't actually take 12 days between a commit change, it was to reflect a change that I actually got this done on October 2nd

--- main.cpp
+++ main.cpp
@@ -22,93 +22,147 @@
 	 {9, "billion"}, {12, "trillion"}, {15, "quadrillion"}, {18, "quintillion"},
 	 {21, "sextillion"}, {24, "septillion"}, {27, "octillion"}, {30, "nondecillion"}};
 
-	enum Type { Ones, Tens, Hundreds, Zero };
+	enum Type { Ones, Tens,  Hundreds, Teens, Zero };
 
 
-std::pair<std::string, bool> partToEnglish(std::string const& str, int idx);
+std::pair<std::string, bool> partToEnglish(std::string const& str, int idx, bool lastPart = false);
 
 
 std::string intToEnglish(long long num) {
-	std::string str = std::to_string(num);
-	std::string number;
+	/*str is the string we are going to iterate
+	over when we do our converting*/
+	std::string const str = std::to_string(num);
+	//this will be the 
+	std::string english;
 
-	/*temp variable for pairs of 3 digits,
-	like thousands, millions, etc, as well
-	as wether or not that part was all zeroes*/
+	/*each "part" is a set of (usually) three digits,
+	that go by a pronoun, like thousands, millions, etc.
+	The bool in part is wether or not the current part
+	had all blank zeroes, in which case we wouldnt give it
+	a pronoun*/
 	std::pair<std::string, bool> part;
-	bool tmp;
+	//wether or not the previous iteration of part was filled with zeroes
+	bool lastEmpty = true;
+	bool negative = (str[0] == '-');
+
+	//lol
+	if (str== "0") return "zero";
+
+	if (negative)
+		english += "negative ";
+
+	//set i to 1 if there is a netgative, else 0
+	int i = negative;
  	
- 	//convert the first part to english
- 	//if (str.size()%3 != 0)
- 	part = partToEnglish(str, 0);
-	number += part.first;
-	tmp = part.second;
+ 	/*Convert the first "part" to english if the string
+ 	 isn't a multilpe of three chars. This way we can
+ 	 iterate over the string three digits at a time using
+ 	 our partToEnglish() function.*/
+ 	if ((str.size()-i) % 3 != 0) {
+ 		part = partToEnglish(str, i);
+		english += part.first;
+		lastEmpty = part.second;
 
-	for (int i = str.size()%3 == 0 ? 3 : str.size()%3; i < str.size(); i+=3) {
-		//put the place value of the current digit at the next hundreds place
+		//go to nearest next part
+		i = (str.size()-negative) % 3 + negative;
+	}
+
+	for (; i < str.size(); i+=3) {
+		/*place value of the current digit. Note "i"
+		is NOT the place value, i is the index. 
+		i (increases) from right to right while placeValue
+		inreases from  left to right.*/
 		int placeValue = str.size() - i;
-		part = partToEnglish(str, i);
+		//get next part()
+		part = partToEnglish(str, i, placeValue==3);
 
-		number += (tmp ? "" : places[placeValue]  + ' ') + part.first;
-		tmp = part.second;
+		english += (lastEmpty ? "" : places[placeValue] + ' ') + part.first;
+		lastEmpty = part.second;
 	}
 
 	//god that took too long to write
-	return number;
+	return english;
 }
 
 
-std::pair<std::string, bool> partToEnglish(std::string const& str, int idx) {
+std::pair<std::string, bool> partToEnglish(std::string const& str, int idx, bool lastPart) {
 	std::string part;
+	//wether or not this part was all zeroes
 	bool emptyFlag = true;
 
+	//trigger break from inside switch statements
+	bool breakFlag = false;
+
+	bool negative = (str[0] == '-');
+
 	//will break in place
-	for (int i = idx;; i++) {
+	for (int i = idx; !breakFlag; i++) {
 
-		char const digit = str[i];
-		/*note this is different from placeValue in the parent
-		function, as this place value is index 0 and the 
-		one in our parent function is index 1*/
+		char digit = str[i];
+		/*note this is VERY different from placeValue in the 
+		parent function, as this place value is index 0 
+		and the  one in our parent function is index 1. 
+		This will also subtract one from the place value
+		if there is a negative*/
 		int placeValue = str.size()-i-1;
-		//ones, tens, or hundreds
-		Type const type = static_cast<Type>(placeValue%3);
+		//ones, tens, hundreds, or zero if theres a zero digit
+		Type type;
 
+		if (digit != '0') {
+			type = static_cast<Type>(placeValue%3);
+		} else {
+			type = Zero;
+		}
+
+		if (digit == '1' && type == Tens) {
+			type = Teens;
+		}
 		/*this is some weird ass code, and I wasn't sure
 		what the best way to format it was... basically,
 		if the digit is zero, don't execute the normal statements,
 		and if the digit is zero in the ones place, break.*/
-		
-		if (digit != '0') {
-			/*at least one of the digits wasn't zero,
-			so it wasn't empty*/
-			emptyFlag = false;
-
-			if (type == Hundreds) {
+		switch (type) {
+			case (Hundreds) :
 				part += ones[digit] + " hundred";
-
-			} else if (type == Tens) {
-				if (digit!='1') {
-					part += tens[digit];
-				} else {
-					//must be teens
-					//need to peek at the next digit to determine what "teen" number it is
-					part += teens[str[i+1]];
-					//get outta here!
-					break;
-				} 
-			} else {
-				//must be ones
-				part += ones[digit];
 				break;
-			}
 
+			case (Tens) :
+				if (lastPart) {
+					part += "and ";
+					//don't repeat for the ones
+					lastPart = false;
+				}
+				part += tens[digit];
+				break;
+			
+			case (Teens) :
+				if (lastPart) 
+					part += "and ";
+
+				part += teens[str[i+1]];
+				//finish after teens
+				breakFlag = true;
+				break;
+			
+			case (Ones) :
+				if (lastPart) 
+					part += "and ";
+
+				part += ones[digit];
+				breakFlag = true;
+				break;
+			
+		}
+
+		if (type != Zero) {
+			emptyFlag = false;
 			part += ' ';
-		} else if (type == Ones) {
-			break;
+		//sketchy way of saying "if digit is 0 and placevalue is ones"
+		} else if (placeValue % 3 == 0) {
+			breakFlag = true;
 		}
 	}
 
-	part += ' ';
 	return {part, emptyFlag};
 }
 
